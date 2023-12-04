f37263331b7cafa7f5e6bc37d37b147f85d2e424
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-05 04:43:15 +0700

    doc: modeling data

---
+++ models/user_model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true 
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
