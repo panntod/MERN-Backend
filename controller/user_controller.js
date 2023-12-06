7d4b5445144a4fc23b5732b792d4b1de64fb5279
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-07 04:19:36 +0700

    refactor: deleted console log

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,38 +1,52 @@
-const { connectDB, closeDB } = require("../config/connection");
+const { connectDB } = require("../config/connection");
 const User = require("../models/user_model");
 
 exports.getUser = async () => {
   await connectDB();
   try {
     const users = await User.find({});
-    console.log(users);
     return users;
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error:", error.message);
     throw error;
   }
 };
 
-exports.addUser = async (name, age, address, email) => {
+exports.addUser = async (name, age, address, email, password) => {
   await connectDB();
   try {
-    const newUser = new User({ name, age, address, email });
+    const newUser = new User({ name, age, address, email, password });
     const insertedUser = await newUser.save();
     return insertedUser;
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error:", error.message);
     throw error;
   }
 };
 
-exports.updateUser = async (id, name, age, address, email) => {
+exports.updateUser = async (id, name, age, address, email, password) => {
   await connectDB();
   try {
+    const existingUser = await User.findOne({ email });
+
+    if(!existingUser){
+      const error = new Error("User Not Found");
+      error.status = 404;
+      throw error;
+    }
+    
+    if (existingUser && existingUser._id.toString() !== id) {
+      const error = new Error("Email already exists");
+      error.status = 409;
+      throw error;
+    }
+
     const updatedUser = await User.findByIdAndUpdate(
       id,
-      { name, age, address, email },
+      { name, age, address, email, password },
       { new: true }
     );
+
     return updatedUser;
   } catch (error) {
     console.error("Error:", error);
@@ -55,5 +69,23 @@ exports.deleteUser = async (id) => {
   } catch (error) {
     console.error("Error:", error);
     throw error;
+  }
+};
+
+exports.loginUser = async (email, password) => {
+  await connectDB();
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return { success: false, message: "User not found" };
+    }
+
+    if (user.password !== password) {
+      return { success: false, message: "Incorrect password" };
+    }
+
+    return { success: true, message: "Login successful", user };
+  } catch (error) {
+    return { success: false, message: "Error logging in", error };
   }
 };
