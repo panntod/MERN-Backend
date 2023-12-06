774f73c02f1e68a1deffae69ba7a9f71f082b81b
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-07 04:21:38 +0700

    doc: added product and photo routes

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const userRoutes = require('./routes/user_route');
+const productRoutes = require('./routes/product_route');
+const photoRoutes = require('./routes/photo_routes');
 
 app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/product', productRoutes);
+app.use('/photo', photoRoutes);
 
-const PORT = 5000; // Ganti dengan port yang Anda inginkan
+const PORT = 5000; 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
