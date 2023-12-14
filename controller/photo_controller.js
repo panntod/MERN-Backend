893eb57e5e66c7237a62e3c0afa7cc168d7d7cfb
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-15 05:27:42 +0700

    feat: deleted await connection

--- controller/photo_controller.js
+++ controller/photo_controller.js
@@ -1,10 +1,8 @@
-const { connectDB } = require("../config/connection");
 const Photo = require("../models/photo_model");
 const fs = require("fs");
 
 // Fungsi untuk mendapatkan semua foto
 exports.getPhotos = async () => {
-  await connectDB();
   try {
     const photos = await Photo.find({});
     return photos;
@@ -14,11 +12,21 @@ exports.getPhotos = async () => {
   }
 };
 
-// Fungsi untuk menambahkan foto baru
-exports.addPhoto = async (namaFile, data) => {
-  await connectDB();
+// Fungsi untuk mendapatkan semua foto
+exports.findPhotoById = async (id) => {
   try {
-    const newPhoto = new Photo({ namaFile, data });
+    const photos = await Photo.findById({id});
+    return photos;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+};
+
+// Fungsi untuk menambahkan foto baru
+exports.addPhoto = async (photoData) => {
+  try {
+    const newPhoto = new Photo({ photoData });
     const insertedPhoto = await newPhoto.save();
     return insertedPhoto;
   } catch (error) {
@@ -27,30 +35,7 @@ exports.addPhoto = async (namaFile, data) => {
   }
 };
 
-exports.updatePhoto = async (id, namaFile, data) => {
-  await connectDB();
-  try {
-    const updatePhoto = await Photo.findByIdAndUpdate(
-      id,
-      { namaFile, data },
-      { new: true }
-    );
-
-    if (!updatePhoto) {
-      const error = new Error("Photo Not Found");
-      error.status = 404;
-      throw error;
-    }
-
-    return updatePhoto;
-  } catch (error) {
-    console.error("Error:", error.message);
-    throw error;
-  }
-};
-
 exports.deletePhoto = async (id) => {
-  await connectDB();
   try {
     const deletedPhoto = await Photo.findOneAndDelete( {_id: id });
 
