34cd65f58a1a2342099d26cef78ee2bcae377843
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-15 05:26:21 +0700

    chore: added function connect db, added express

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const { connectDB } = require("./config/connection");
+
 const userRoutes = require('./routes/user_route');
 const productRoutes = require('./routes/product_route');
 const photoRoutes = require('./routes/photo_routes');
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+
 app.use('/users', userRoutes);
 app.use('/product', productRoutes);
 app.use('/photo', photoRoutes);
 
 const PORT = 5000; 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB( app, PORT )
